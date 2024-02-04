8f71ada71cfade3162fbfafbc042218da03e7d33
Aniket shrivastava <aniket.shrivastava91@gmail.com>
2024-02-04 23:48:32 +0530

    update component Ui

--- React-router-project/src/components/Header/Header.jsx
+++ React-router-project/src/components/Header/Header.jsx
@@ -34,6 +34,7 @@ function Header() {
                 <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                     <li>
                         <NavLink
+                        to="/"
                             className={({isActive}) =>
                                 `block py-2 pr-4 pl-3 duration-200 ${isActive?"text-orange-700":"text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                             }
@@ -41,6 +42,36 @@ function Header() {
                             Home
                         </NavLink>
                     </li>
+                    <li>
+                        <NavLink
+                        to="/about"
+                            className={({isActive}) =>
+                                `block py-2 pr-4 pl-3 duration-200 ${isActive?"text-orange-700":"text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                            }
+                        >
+                            About
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                        to="/contact"
+                            className={({isActive}) =>
+                                `block py-2 pr-4 pl-3 duration-200 ${isActive?"text-orange-700":"text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                            }
+                        >
+                            Contact
+                        </NavLink>
+                    </li>
+                    <li>
+                    <NavLink
+                    to="/github"
+                        className={({isActive}) =>
+                            `block py-2 pr-4 pl-3 duration-200 ${isActive?"text-orange-700":"text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                        }
+                    >
+                        Github
+                    </NavLink>
+                </li>
                     
                     
                 </ul>
